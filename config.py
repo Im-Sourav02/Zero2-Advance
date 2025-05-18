4bb6631bff4aa0e0152e9a18ef4ac72981194b21
Sourav Agarwal <souravagarwal14092007@gmail.com>
2025-05-18 22:01:21 +0530

    rename db name in the congig file

--- config.py
+++ config.py
@@ -17,7 +17,7 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "6123108288")) # Owner id
 PORT = os.environ.get("PORT", "8001")
 #--------------------------------------------
 DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://souravagarwal14092007:szXRs8g7fErCnn4@cluster0.xlsbf3o.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DB_NAME = os.environ.get("DATABASE_NAME", "Zero 2")
+DB_NAME = os.environ.get("DATABASE_NAME", "Clustor0")
 #--------------------------------------------
 FSUB_LINK_EXPIRY = int(os.getenv("FSUB_LINK_EXPIRY", "10"))  # 0 means no expiry
 BAN_SUPPORT = os.environ.get("BAN_SUPPORT", "@Im_Sukuna02")
